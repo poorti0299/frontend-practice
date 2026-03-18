aaabaf1c964bba7b118bb355e7787b3db58daad9
Poorti Maheshwari <Poorti.Maheshwari@coxautoinc.com>
2026-03-18 12:07:23 +0530

    React App with Tailwind CSS and Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomHookPattern from "./feature/customHookPattern";
+
+function App() {
+
+  return (
+    <div>
+      <h1>Different Design Patterns Example</h1>
+      <CustomHookPattern />
+    </div>
+  )
+}
+
+export default App
