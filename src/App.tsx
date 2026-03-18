ba05c2e89d39616205794b1b84ceff5aa7185372
Poorti Maheshwari <Poorti.Maheshwari@coxautoinc.com>
2026-03-18 12:20:48 +0530

    Fixed the error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import './App.css';
-import CustomHookPattern from "./feature/customHookPattern";
-
 function App() {
 
   return (
     <div>
       <h1>Different Design Patterns Example</h1>
-      <CustomHookPattern />
     </div>
   )
 }
